0caa21e907f42f071b8baeeb70faa1e17d321943
pangsonyo <1015614034@qq.com>
2018-10-19 16:47:26 +0800

    Update UtilTest.java

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -5,7 +5,10 @@
 public class UtilTest {
 
     public static void main(String[] args) {
+         //stage1
          //Util.ifFizzBuzzOrBuzz(100);
+        
+         //stage2
          Util.ifFizzBuzzOrBuzzStage2(100);
 
     }
