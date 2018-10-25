2c97df9c7188ef72a2e5bfc1db32be427cb4d48e
pangsonyo <1015614034@qq.com>
2018-10-25 11:12:44 +0800

    Update BuzzPredicate.java

--- src/main/java/com/impl/BuzzPredicate.java
+++ src/main/java/com/impl/BuzzPredicate.java
@@ -4,10 +4,6 @@ import com.ipredocate.Predicate;
 
 public class BuzzPredicate implements Predicate {
 
-    public boolean rule(Integer i) {
-        return  i % 3 == 0;
-    }
-
     public String behavior(Integer i) {
         return  "Buzz";
     }
