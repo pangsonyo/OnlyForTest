5af7cf5752b5a6119fac3b1a2c422851aa352377
pangsonyo <1015614034@qq.com>
2018-10-25 11:13:08 +0800

    Update FizzBuzzPredicate.java

--- src/main/java/com/impl/FizzBuzzPredicate.java
+++ src/main/java/com/impl/FizzBuzzPredicate.java
@@ -4,11 +4,6 @@ import com.ipredocate.Predicate;
 
 public class FizzBuzzPredicate implements Predicate {
 
-
-    public boolean rule(Integer i) {
-        return i % 15 == 0;
-    }
-
     public String behavior(Integer i) {
         return  "FizzBuzz";
     }
